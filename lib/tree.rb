77cb25520c3a8732b7d3307845c0c950d5072d4f
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-07-11 15:19:15 -0400

    commit tree a10bc74059d1f865168576cc43aac1bdfbaffdae

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,4 +2,19 @@ class Tree
   def initialize(root = nil)
     @root = root
   end
+
+  def get_element_by_id(str)
+    nodes_to_visit = [@root]
+    current_node = nil
+    while nodes_to_visit.length > 0
+      current_node = nodes_to_visit.shift
+      if current_node[:id] == str
+        return current_node
+      else
+        nodes_to_visit = nodes_to_visit + current_node[:children]
+      end
+    end
+
+    return nil
+  end
 end
